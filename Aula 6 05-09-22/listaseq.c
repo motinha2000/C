e9a9b9a18f51805e9f148ba5abc6613e94f9b1f0
Gabriel.M <gamotasantos2000@gmail.com>
2022-09-05 17:08:39 -0400

    aprendendo sobre listas sequenciais

--- Aula 6 05-09-22/listaseq.c
+++ Aula 6 05-09-22/listaseq.c
@@ -1,2 +1,61 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "listaseq.h"
+
+struct lista
+{
+    int qtd;
+    struct aluno dados[MAX];
+};
+
+// CRIANDO UMA LISTA
+
+Lista *cria_lista()
+{
+    Lista *li;
+    li = (Lista *)malloc(sizeof(struct lista));
+    if (li != NULL)
+        li->qtd = 0;
+    return li;
+}
+
+void libera_lista(Lista *li)
+{
+    free(li);
+}
+
+int tamanho_lista(Lista *li)
+{
+    if (li == NULL)
+        return -1;
+    else
+        return li->qtd;
+}
+
+int lista_cheia(Lista *li)
+{
+    if (li == NULL)
+        return -1;
+    return (li->qtd == MAX);
+}
+
+int lista_vazia(Lista *li)
+{
+    if (li == NULL)
+        return -1;
+    return (li->qtd == 0);
+}
+
+int insere_lista_inicio(Lista *li, struct aluno al)
+{
+    if(li==NULL)
+        return 0;
+    if(li->qtd==MAX)
+        return 0;
+    int i;
+    for(li->qtd-1;i>=0;i--)
+        li->dados[i+1]=li->dados[i];
+    li->dados[0]=al;
+    li->qtd++;
+    return 1;
+}
--- Aula 6 05-09-22/listaseq.h
+++ Aula 6 05-09-22/listaseq.h
@@ -1,24 +1,24 @@
 #define MAX 100
 
-struct aluno {
+struct aluno
+{
     int matricula;
     char nome[30];
-    float n1,n2,n3;
-
+    float n1, n2, n3;
 };
 
 typedef struct lista Lista;
 
-Lista* cria_lista();
-void libera_lista(Lista *li);
+Lista *cria_lista();          // PRONTO
+void libera_lista(Lista *li); // PRONTO
 int busca_lista_pos(Lista *li, int pos, struct aluno *al);
 int busca_lista_mat(Lista *li, int mat, struct aluno *al);
 int insere_lista_final(Lista *li, struct aluno al);
 int insere_lista_ordenada(Lista *li, struct aluno al);
-int insere_lista_inicio(Lista *li, struct aluno al);
+int insere_lista_inicio(Lista *li, struct aluno al); //PRONTO
 int remove_lista(Lista *li, int mat);
 int remove_lista_inicio(Lista *li);
 int remove_lista_final(Lista *li);
-int tamanho_lista(Lista li);
-int lista_cheia(Lista *li);
-int lista_vazia(Lista *li);
+int tamanho_lista(Lista *li); // PRONTO
+int lista_cheia(Lista *li);  // PRONTO
+int lista_vazia(Lista *li);  // PRONTO
